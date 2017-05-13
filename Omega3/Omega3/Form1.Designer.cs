4d3319afe04fd6bd6243d76fa28d503bb367396e
julian argañaraz <hiory.cs@hotmail.com>
2017-05-13 20:55:13 -0300

    puse cosa

--- Omega3/Omega3/Form1.Designer.cs
+++ Omega3/Omega3/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(148, 100);
+            this.button1.Location = new System.Drawing.Point(31, 217);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
