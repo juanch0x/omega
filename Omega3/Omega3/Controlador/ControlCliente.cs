24d0ced619191ddab490ea69fefef0002b795656
julian argañaraz <hiory.cs@hotmail.com>
2017-05-23 23:35:21 -0300

    todos los abm

---
+++ Omega3/Omega3/Controlador/ControlCliente.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Omega3.Modelo;
+
+namespace Omega3.Controlador
+{
+    class ControlCliente
+    {
+        public static int AgregarCliente(Cliente cliente)
+        {
+
+            int retorno = 0;
+
+            MySqlCommand comando = new MySqlCommand(string.Format("Insert into Clientes (nombre, telefono, direccion, provincia, localidad, email) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}')",
+                cliente.Nombre_cliente, cliente.Telefono, cliente.Direccion, cliente.Provincia, cliente.Localidad, cliente.Email), Conexion.ObtenerConexion());
+            retorno = comando.ExecuteNonQuery();
+            return retorno;
+        }
+
+        public static int ModificarCliente(Cliente cliente)
+        {
+            int retorno = 0;
+            MySqlConnection conexion = Conexion.ObtenerConexion();
+
+            MySqlCommand comando = new MySqlCommand(string.Format("Update Clientes set nombre='{1}', telefono='{2}', direccion='{3}', provincia='{4}', localidad='{5}', email='{6}' where id_cliente={0}",
+                cliente.Id_cliente, cliente.Nombre_cliente, cliente.Telefono, cliente.Direccion, cliente.Provincia, cliente.Localidad, cliente.Email), conexion);
+
+            retorno = comando.ExecuteNonQuery();
+            conexion.Close();
+
+            return retorno;
+
+
+        }
+        public static int EliminarCliente(Cliente cliente)
+        {
+            int retorno = 0;
+            MySqlConnection conexion = Conexion.ObtenerConexion();
+
+            MySqlCommand comando = new MySqlCommand(string.Format("DELETE FROM Clientes where id_cliente='{0}'",
+                cliente.Id_cliente), conexion);
+
+            retorno = comando.ExecuteNonQuery();
+            conexion.Close();
+
+            return retorno;
+
+        }
+    }
+}
---
+++ Omega3/Omega3/Controlador/ControlProveedor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Omega3.Modelo;
+
+
+namespace Omega3.Controlador
+{
+    class ControlProveedor
+    {
+        public static int AgregarProveedor(Proveedor proveedor)
+        {
+
+            int retorno = 0;
+
+            MySqlCommand comando = new MySqlCommand(string.Format("Insert into Proveedores (nombre, telefono, direccion, provincia, localidad, email) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}')",
+                proveedor.Nombre_proveedor, proveedor.Telefono, proveedor.Direccion, proveedor.Provincia, proveedor.Localidad, proveedor.Email), Conexion.ObtenerConexion());
+            retorno = comando.ExecuteNonQuery();
+            return retorno;
+        }
+
+        public static int ModificarCliente(Proveedor proveedor)
+        {
+            int retorno = 0;
+            MySqlConnection conexion = Conexion.ObtenerConexion();
+
+            MySqlCommand comando = new MySqlCommand(string.Format("Update Proveedores set nombre='{1}', telefono='{2}', direccion='{3}', provincia='{4}', localidad='{5}', email='{6}' where id_proveedor={0}",
+                proveedor.Id_proveedor, proveedor.Nombre_proveedor, proveedor.Telefono, proveedor.Direccion, proveedor.Provincia, proveedor.Localidad, proveedor.Email), conexion);
+
+            retorno = comando.ExecuteNonQuery();
+            conexion.Close();
+
+            return retorno;
+
+
+        }
+        public static int EliminarProveedor(Proveedor proveedor)
+        {
+            int retorno = 0;
+            MySqlConnection conexion = Conexion.ObtenerConexion();
+
+            MySqlCommand comando = new MySqlCommand(string.Format("DELETE FROM Proveedores where id_proveedor='{0}'",
+                proveedor.Id_proveedor), conexion);
+
+            retorno = comando.ExecuteNonQuery();
+            conexion.Close();
+
+            return retorno;
+
+        }
+    }
+}
+}
