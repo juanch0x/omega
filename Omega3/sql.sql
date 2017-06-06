f17ce1e608ae6cf86de1a8a7302049b365abb54a
juanch0x <thejuasz@gmail.com>
2017-06-05 22:18:59 -0300

    Sql update

--- Omega3/sql.sql
+++ Omega3/sql.sql
@@ -1,71 +1,118 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.5
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
--- Host: sql10.freesqldatabase.com
--- Generation Time: Jun 01, 2017 at 02:54 PM
--- Server version: 5.5.53-0ubuntu0.14.04.1
--- PHP Version: 5.3.28
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-06-2017 a las 03:18:27
+-- Versión del servidor: 10.1.21-MariaDB
+-- Versión de PHP: 5.6.30
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sql10176067`
+-- Base de datos: `omega`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Productos`
+-- Estructura de tabla para la tabla `productos`
 --
 
-CREATE TABLE IF NOT EXISTS `Productos` (
-  `cod_producto` int(10) NOT NULL AUTO_INCREMENT,
-  `producto` varchar(20) NOT NULL,
-  `cantidad` int(3) NOT NULL,
-  `precio_compra` decimal(6,4) NOT NULL,
-  `precio_venta` decimal(6,4) NOT NULL,
-  `stock_minimo` int(4) NOT NULL,
-  PRIMARY KEY (`cod_producto`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `Productos`
---
-
-INSERT INTO `Productos` (`cod_producto`, `producto`, `cantidad`, `precio_compra`, `precio_venta`, `stock_minimo`) VALUES
-(1, 'Wea', 2, '99.9999', '99.1110', 0),
-(2, 'Wea', 2, '99.9999', '99.1110', 0),
-(3, 'Wea', 2, '99.9999', '99.1110', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usuarios`
---
-
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `usuario` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `id_rol` int(3) NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  PRIMARY KEY (`usuario`)
+CREATE TABLE `productos` (
+  `cod_producto` int(200) NOT NULL,
+  `producto` varchar(200) NOT NULL,
+  `cantidad` int(10) NOT NULL,
+  `precio_compra` decimal(10,2) NOT NULL,
+  `precio_venta` decimal(10,2) NOT NULL,
+  `stock_minimo` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `usuarios`
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`cod_producto`, `producto`, `cantidad`, `precio_compra`, `precio_venta`, `stock_minimo`) VALUES
+(0, 'pepe en patinetas', 0, '292.20', '994.20', 1919),
+(20, 'Gilada', 0, '200.00', '25.15', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(3) NOT NULL,
+  `nombre_rol` varchar(10) NOT NULL,
+  `descripcion_rol` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `nombre_rol`, `descripcion_rol`) VALUES
+(1, 'Ventas', 'asdsaddsadsd');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usuario` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `id_rol` int(3) NOT NULL,
+  `nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`usuario`, `password`, `id_rol`, `nombre`) VALUES
 ('juanch0x', 'asdqwe123', 1, 'Juan Portugal');
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`cod_producto`);
+
+--
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
