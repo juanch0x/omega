fe1be5ecbd03ec42e74020b1d1dd0c2781475222
Juan Portugal <juanch0x@live.com>
2017-06-12 11:51:48 -0300

    Arme la interfaz del agregarproveedores, falta validar los campos y agregar la funcionalidad

---
+++ Omega3/Omega3/Vista/Proveedores/AgregarProveedores.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Omega3.Vista.Proveedores
+{
+    partial class AgregarProveedores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_proveedor = new System.Windows.Forms.Label();
+            this.txt_proveedor = new System.Windows.Forms.TextBox();
+            this.lbl_telefono = new System.Windows.Forms.Label();
+            this.txt_telefono = new System.Windows.Forms.TextBox();
+            this.lbl_direccion = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_provincia = new System.Windows.Forms.Label();
+            this.txt_provincia = new System.Windows.Forms.TextBox();
+            this.txt_codigo_postal = new System.Windows.Forms.TextBox();
+            this.lbl_codigo_postal = new System.Windows.Forms.Label();
+            this.lbl_email = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_proveedor
+            // 
+            this.lbl_proveedor.AutoSize = true;
+            this.lbl_proveedor.Location = new System.Drawing.Point(12, 22);
+            this.lbl_proveedor.Name = "lbl_proveedor";
+            this.lbl_proveedor.Size = new System.Drawing.Size(56, 13);
+            this.lbl_proveedor.TabIndex = 0;
+            this.lbl_proveedor.Text = "Proveedor";
+            // 
+            // txt_proveedor
+            // 
+            this.txt_proveedor.Location = new System.Drawing.Point(90, 19);
+            this.txt_proveedor.Name = "txt_proveedor";
+            this.txt_proveedor.Size = new System.Drawing.Size(160, 20);
+            this.txt_proveedor.TabIndex = 1;
+            // 
+            // lbl_telefono
+            // 
+            this.lbl_telefono.AutoSize = true;
+            this.lbl_telefono.Location = new System.Drawing.Point(12, 58);
+            this.lbl_telefono.Name = "lbl_telefono";
+            this.lbl_telefono.Size = new System.Drawing.Size(49, 13);
+            this.lbl_telefono.TabIndex = 2;
+            this.lbl_telefono.Text = "Teléfono";
+            // 
+            // txt_telefono
+            // 
+            this.txt_telefono.Location = new System.Drawing.Point(90, 51);
+            this.txt_telefono.Name = "txt_telefono";
+            this.txt_telefono.Size = new System.Drawing.Size(160, 20);
+            this.txt_telefono.TabIndex = 3;
+            // 
+            // lbl_direccion
+            // 
+            this.lbl_direccion.AutoSize = true;
+            this.lbl_direccion.Location = new System.Drawing.Point(12, 87);
+            this.lbl_direccion.Name = "lbl_direccion";
+            this.lbl_direccion.Size = new System.Drawing.Size(52, 13);
+            this.lbl_direccion.TabIndex = 4;
+            this.lbl_direccion.Text = "Dirección";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // lbl_provincia
+            // 
+            this.lbl_provincia.AutoSize = true;
+            this.lbl_provincia.Location = new System.Drawing.Point(12, 116);
+            this.lbl_provincia.Name = "lbl_provincia";
+            this.lbl_provincia.Size = new System.Drawing.Size(51, 13);
+            this.lbl_provincia.TabIndex = 6;
+            this.lbl_provincia.Text = "Provincia";
+            // 
+            // txt_provincia
+            // 
+            this.txt_provincia.Location = new System.Drawing.Point(90, 116);
+            this.txt_provincia.Name = "txt_provincia";
+            this.txt_provincia.Size = new System.Drawing.Size(160, 20);
+            this.txt_provincia.TabIndex = 7;
+            // 
+            // txt_codigo_postal
+            // 
+            this.txt_codigo_postal.Location = new System.Drawing.Point(90, 142);
+            this.txt_codigo_postal.Name = "txt_codigo_postal";
+            this.txt_codigo_postal.Size = new System.Drawing.Size(160, 20);
+            this.txt_codigo_postal.TabIndex = 8;
+            // 
+            // lbl_codigo_postal
+            // 
+            this.lbl_codigo_postal.AutoSize = true;
+            this.lbl_codigo_postal.Location = new System.Drawing.Point(12, 142);
+            this.lbl_codigo_postal.Name = "lbl_codigo_postal";
+            this.lbl_codigo_postal.Size = new System.Drawing.Size(72, 13);
+            this.lbl_codigo_postal.TabIndex = 9;
+            this.lbl_codigo_postal.Text = "Codigo Postal";
+            // 
+            // lbl_email
+            // 
+            this.lbl_email.AutoSize = true;
+            this.lbl_email.Location = new System.Drawing.Point(12, 173);
+            this.lbl_email.Name = "lbl_email";
+            this.lbl_email.Size = new System.Drawing.Size(35, 13);
+            this.lbl_email.TabIndex = 10;
+            this.lbl_email.Text = "E-mail";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(90, 170);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(160, 20);
+            this.txt_email.TabIndex = 11;
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.Location = new System.Drawing.Point(23, 215);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(75, 23);
+            this.btn_agregar.TabIndex = 12;
+            this.btn_agregar.Text = "Agregar";
+            this.btn_agregar.UseVisualStyleBackColor = true;
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(104, 215);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(75, 23);
+            this.btn_limpiar.TabIndex = 13;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(185, 215);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(75, 23);
+            this.btn_salir.TabIndex = 14;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            // 
+            // AgregarProveedores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_agregar);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.lbl_email);
+            this.Controls.Add(this.lbl_codigo_postal);
+            this.Controls.Add(this.txt_codigo_postal);
+            this.Controls.Add(this.txt_provincia);
+            this.Controls.Add(this.lbl_provincia);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_direccion);
+            this.Controls.Add(this.txt_telefono);
+            this.Controls.Add(this.lbl_telefono);
+            this.Controls.Add(this.txt_proveedor);
+            this.Controls.Add(this.lbl_proveedor);
+            this.Name = "AgregarProveedores";
+            this.Text = "Agregar Proveedores";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_proveedor;
+        private System.Windows.Forms.TextBox txt_proveedor;
+        private System.Windows.Forms.Label lbl_telefono;
+        private System.Windows.Forms.TextBox txt_telefono;
+        private System.Windows.Forms.Label lbl_direccion;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_provincia;
+        private System.Windows.Forms.TextBox txt_provincia;
+        private System.Windows.Forms.TextBox txt_codigo_postal;
+        private System.Windows.Forms.Label lbl_codigo_postal;
+        private System.Windows.Forms.Label lbl_email;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
